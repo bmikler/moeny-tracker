88ee10693263ef08688efe4a851ab4687a7c1feb
bmikler <bartlomiej.mikler@gmail.com>
2023-11-18 14:35:53 +0100

    init commit

--- app/src/main/java/com/android/moneytracker/ui/MoneyTrackerApp.kt
+++ app/src/main/java/com/android/moneytracker/ui/MoneyTrackerApp.kt
@@ -1,9 +1,20 @@
 package com.android.moneytracker.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.android.moneytracker.R
 import com.android.moneytracker.ui.navigation.MoneyTrackerNavHost
 
 @Composable
@@ -13,4 +24,31 @@ fun MoneyTrackerApp(
     Column {
         MoneyTrackerNavHost(navController = navController)
     }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MoneyTrackerTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.btn_back)
+                    )
+                }
+            }
+        }
+    )
 }
--- app/src/main/java/com/android/moneytracker/ui/expenses/ExpenseScreen.kt
+++ app/src/main/java/com/android/moneytracker/ui/expenses/ExpenseScreen.kt
@@ -1,16 +1,16 @@
 package com.android.moneytracker.ui.expenses
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.android.moneytracker.R
+import com.android.moneytracker.ui.MoneyTrackerTopAppBar
 import com.android.moneytracker.ui.navigation.NavigationDestination
-import kotlinx.coroutines.launch
+
 
 object ExpenseDestination : NavigationDestination {
     override val route: String = "spending"
@@ -20,29 +20,24 @@ object ExpenseDestination : NavigationDestination {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ExpenseScreen() {
+fun ExpenseScreen(
+    modifier: Modifier = Modifier,
+) {
 
     Scaffold(
         topBar = {
-            EduTrackerTopAppBar(
-                title = title,
+            MoneyTrackerTopAppBar(
+                title = stringResource(id = ExpenseDestination.titleRes),
                 canNavigateBack = true,
-                navigateUp = navigateBack
+                navigateUp = {  }
             )
         }) { innerPadding ->
-        ItemEntryBody(
-            groupUiState = viewModel.groupUiState,
-            onItemValueChange = { viewModel.updateUiState(it) },
-            onSaveClick = {
-                coroutineScope.launch { viewModel.saveGroup() }
-                navigateBack()
-            },
-            isButtonEnabled = viewModel.isEntryValid(),
-            modifier = Modifier
-                .padding(innerPadding)
-                .verticalScroll(rememberScrollState())
-                .fillMaxWidth()
-        )
+            ExpenseBody(modifier = modifier.padding(innerPadding))
     }
 
+}
+
+@Composable
+fun ExpenseBody(modifier: Modifier = Modifier) {
+    Text(text = "hello")
 }
