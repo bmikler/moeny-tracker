99eb9ca41c55704007cc369eef580cdfaa18cbed
bmikler <bartlomiej.mikler@gmail.com>
2023-12-03 15:12:07 +0100

    clean up expense view model and icon changed

--- app/src/main/java/com/android/moneytracker/ui/expense/ExpenseViewModel.kt
+++ app/src/main/java/com/android/moneytracker/ui/expense/ExpenseViewModel.kt
@@ -22,68 +22,63 @@ class ExpenseViewModel(
     private val sharedDateViewModel: SharedDateViewModel
 ) : ViewModel() {
 
-    private lateinit var categories: List<Category>
-    private lateinit var expenses: List<Expense>
-    private lateinit var annualExpensesSummed: List<CategoryTotal>
 
-    private val _uiState = MutableStateFlow(ExpenseUiState(sharedDateViewModel.date.toString(), mapOf()))
+    private val _uiState =
+        MutableStateFlow(ExpenseUiState(sharedDateViewModel.date.toString(), mapOf()))
     val uiState: StateFlow<ExpenseUiState> = _uiState.asStateFlow()
 
     init {
-        updateUiState()
+        reloadUiState()
     }
 
     fun nextDate() {
         sharedDateViewModel.nextMonth()
-        updateUiState()
+        reloadUiState()
     }
 
     fun previousDate() {
         sharedDateViewModel.previousMonth()
-        updateUiState()
+        reloadUiState()
     }
 
-    private fun updateUiState() {
+    private fun reloadUiState() {
         viewModelScope.launch(Dispatchers.IO) {
-            categories = expenseRepository.getCategories()
-            expenses = expenseRepository.getExpensesForDates(sharedDateViewModel.date.getMonthRange())
-            annualExpensesSummed = expenseRepository.getAnnualExpensesSummedByCategory(sharedDateViewModel.date.getYearRange())
+            val categories = expenseRepository.getCategories()
+            val expenses = expenseRepository.getExpensesForDates(sharedDateViewModel.date.getMonthRange())
+            val expensesByCategoryAnnual = expenseRepository.getAnnualExpensesSummedByCategory(sharedDateViewModel.date.getYearRange())
 
-            val expensesByCategory = categories.associateWith { category -> expenses.filter { expense -> expense.categoryId == category.id } }
+            val expensesByCategory: Map<Category, List<Expense>> = associateCategoriesWithExpenses(categories, expenses)
+
+            val mapKeys: Map<CategoryUi, List<Expense>> =
+                expensesByCategory.mapKeys { (category, expenses) ->
+
+                    val alreadySpent = when (category.type) {
+                        ExpenseType.MONTHLY -> expenses.sumOf { it.value }
+                        ExpenseType.ANNUAL -> expensesByCategoryAnnual.getTotalForCategory(category.id)
+                    }
+
+                    CategoryUi(
+                        id = category.id,
+                        name = category.name,
+                        alreadySpent = alreadySpent,
+                        leftToSpent = category.spendingLimit.minus(alreadySpent),
+                        type = category.type
+                    )
+                }
+
 
             _uiState.update {
-                ExpenseUiState(sharedDateViewModel.date.getString(), mapToUiCategories(expensesByCategory))
+                ExpenseUiState(sharedDateViewModel.date.getString(), mapKeys)
             }
         }
     }
 
-    private fun mapToUiCategories(expensesWithCategories: Map<Category, List<Expense>>): Map<CategoryUi, List<Expense>> {
-        return expensesWithCategories.mapKeys { (category, expenses) ->
-
-                val spentInCurrentMonth = expenses.sumOf { it.value }
-
-                val leftToSpent = when (category.type) {
-                    ExpenseType.MONTHLY -> category.spendingLimit.minus(spentInCurrentMonth)
-                    ExpenseType.ANNUAL -> category.spendingLimit.minus((annualExpensesSummed.firstOrNull { it.id == category.id }?.total) ?: BigDecimal.ZERO)
-                }
-
-                CategoryUi(
-                    id = category.id,
-                    name = category.name,
-                    alreadySpent = spentInCurrentMonth,
-                    leftToSpent = leftToSpent,
-                    type = category.type
-                )
-            }
-    }
-
-
+    private fun associateCategoriesWithExpenses(categories: List<Category>, expenses: List<Expense>) =
+        categories.associateWith { category -> expenses.filter { expense -> expense.categoryId == category.id } }
+    private fun List<CategoryTotal>.getTotalForCategory(id: Int) = (firstOrNull { it.id == id }?.total) ?: BigDecimal.ZERO
     private fun LocalDate.getString() = "$month - $year"
-    private fun LocalDate.getMonthRange() =
-        LocalDate.of(year, month, 1) to LocalDate.of(year, month, lengthOfMonth())
-
-    private fun LocalDate.getYearRange() =
-        LocalDate.of(year, 1, 1) to LocalDate.of(year, 12, lengthOfMonth())
+    private fun LocalDate.getMonthRange() = LocalDate.of(year, month, 1) to LocalDate.of(year, month, lengthOfMonth())
+    private fun LocalDate.getYearRange() = LocalDate.of(year, 1, 1) to LocalDate.of(year, 12, lengthOfMonth())
 }
 
 
@@ -91,7 +86,6 @@ data class ExpenseUiState(
     val date: String,
     val expensesByCategory: Map<CategoryUi, List<Expense>>
 )
-
 
 data class CategoryUi(
     val id: Int,
